66b9634b0da41bf2664d44c9f9c4e3b067db74e9
casual-karma-e108 <akravecas@yahoo.com>
2021-02-18 00:20:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,36 @@
 # Write your methods here
+require 'pry'
 
 def loop_message_five_times (message)
-  counter = 0
-  while counter < 5
+  count = 0
+  while count < 5
     puts(message)
     count += 1
   end
+end
+
+def loop_message_n_times (message, limit)
+  count = 0
+  while count < limit do
+    puts(message)
+    count += 1
+  end
+end
+
+def output_array(array)
+  count=0
+  while count < array.size do
+    puts array[count]
+    count += 1
+  end
+end
+
+def return_string_array(array)
+  count=0
+  new_array =[]
+  while count < array.size do
+    new_array.push(array[count].to_s)
+    count += 1
+  end
+  new_array
 end
